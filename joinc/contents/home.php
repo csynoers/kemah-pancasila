adef3e151bbef7806ac169904b851d1207997634
csynoers <3s0c9m7@gmail.com>
2020-01-20 10:53:07 +0700

    remove animation after effect

--- joinc/contents/home.php
+++ joinc/contents/home.php
@@ -22,14 +22,14 @@
             </div>
         </div>
         <!-- animations effects -->
-        <div class="icon-effects-w3l">
+        <!-- <div class="icon-effects-w3l">
             <img src="images/shape1.png" alt="" class="img-fluid shape-wthree shape-w3-one">
             <img src="images/shape2.png" alt="" class="img-fluid shape-wthree shape-w3-two">
             <img src="images/shape3.png" alt="" class="img-fluid shape-wthree shape-w3-three">
             <img src="images/shape5.png" alt="" class="img-fluid shape-wthree shape-w3-four">
             <img src="images/shape4.png" alt="" class="img-fluid shape-wthree shape-w3-five">
             <img src="images/shape6.png" alt="" class="img-fluid shape-wthree shape-w3-six">
-        </div>
+        </div> -->
         <!-- //animations effects -->
     </div>
 <!-- //banner -->
